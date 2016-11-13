89adbbc1b1d752dade01eb36ff37e390bee1df1e
sanjeet05 <kumarsanjeet1990@gmail.com>
2016-11-13 10:24:21 +0530

    completed

--- shopping-list/js/app.js
+++ shopping-list/js/app.js
@@ -3,18 +3,24 @@ var app = angular.module("shoppingList", []);
 app.controller("myCtrl", function($scope) {
     $scope.products = ["Milk", "Bread", "Cheese"];
     $scope.addItem = function() {
-        $scope.errortext = "";
+        // $scope.errortext = "";
+        $scope.errorText = false;
+        $scope.removeText = false;
         if (!$scope.addMe) {
             return;
         }
         if ($scope.products.indexOf($scope.addMe) == -1) {
             $scope.products.push($scope.addMe);
+            $scope.addMe = "";
         } else {
-            $scope.errortext = "The item is already in your shopping list.";
+            // $scope.errortext = "The item is already in your shopping list.";
+            $scope.errorText = true;
+            $scope.addMe = "";
         }
     };
     $scope.removeItem = function(x) {
-        $scope.errortext = "";
+        // $scope.errortext = "";
         $scope.products.splice(x, 1);
+        $scope.removeText = true;
     };
 });
